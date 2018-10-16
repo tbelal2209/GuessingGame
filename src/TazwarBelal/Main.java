f9f299e0384d40105afa591c5fcb9d7d1589ec29
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-15 20:32:07 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -1,22 +1,27 @@
 package TazwarBelal;
 import java.util.Scanner;
-public class Main {
-
+public class Part2 {
     public static void main(String[] args) {
-	// write your code here
-
+        int i = 0;
         System.out.println("Enter a number");
-            Scanner input= new Scanner(System.in);
+        Scanner input= new Scanner(System.in);
         int num = Integer.parseInt(input.nextLine());
-            int sum = 0;
-            int i = 0;
-            while(num >= i){
-                sum = sum + i;
-                i++;
+        int y=0;
+        while (!(y == num)) {
+            double x = Math.random();
+            y = (int) (x * 101);
+            i++;
+            if (num > y) {
+                System.out.println("your number is too high");
             }
-           System.out.println(sum);
-
+            else if (num < y) {
+                System.out.println("your number is too low");
+            }
 
         }
+        System.out.println("The computer guessed"+" "+num+" "+ "with"+" "+i+" "+"guesses");
+
     }
+    }
+
 
